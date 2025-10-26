3279c406f25af188f101423474527a1e4fb1a2f4
Cristian Pacheco <cristianfpg@gmail.com>
2025-10-26 16:55:56 -0500

    project setup

---
+++ calculadora.py
@@ -0,0 +1,11 @@
+import add, subtract, multiply, divide from calculos-dev1
+import power, square_root, modulo from calculos-dev2
+
+def addAndPower(a, b, exponent):
+    return
+
+def subtractAndRoot(a, b):
+    return
+
+def multiplyAndModulo(a, b, mod):
+    return
---
+++ calculos-dev1.py
@@ -0,0 +1,14 @@
+def add(a, b):
+    return
+
+
+def subtract(a, b):
+    return
+
+
+def multiply(a, b):
+    return
+
+
+def divide(a, b):
+    return
