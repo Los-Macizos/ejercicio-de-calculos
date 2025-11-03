4cc03c300a4dacdc61c9a74161a4c242a2285837
Cristian Arango <kaburo1032@gmail.com>
2025-11-03 14:14:19 -0500

    funciones_dev1

--- calculadora.py
+++ calculadora.py
@@ -2,10 +2,13 @@ from calculos_dev1 import add, subtract, multiply, divide
 from calculos_dev2 import power, square_root, modulo 
 
 def addAndPower(a, b, exponent):
+    suma = add(a, b)
     return
 
 def subtractAndRoot(a, b):
+    resultado = subtract(a, b)
     return
 
 def multiplyAndModulo(a, b, mod):
+    producto = multiply(a, b)
     return
